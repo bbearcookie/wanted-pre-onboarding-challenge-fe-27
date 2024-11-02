c183eadd4fd851f73836b98694f10f4f5bfccedd
bbearcookie <aodem@naver.com>
2024-11-02 17:34:54 +0900

    feat: invariant 유틸 추가

---
+++ src/utils/invariant.ts
@@ -0,0 +1,26 @@
+/**
+ * forked from https://github.com/alexreardon/tiny-invariant/blob/master/src/tiny-invariant.ts
+ */
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const isProduction: boolean = process.env.NODE_ENV === 'production';
+const prefix: string = 'Invariant failed';
+
+export default function invariant(
+  condition: any,
+  message?: string | (() => string),
+): asserts condition {
+  if (condition) {
+    return;
+  }
+
+  if (isProduction) {
+    throw new Error(prefix);
+  }
+
+  const provided: string | undefined =
+    typeof message === 'function' ? message() : message;
+
+  const value: string = provided ? `${prefix}: ${provided}` : prefix;
+  throw new Error(value);
+}
